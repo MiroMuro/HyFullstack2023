ebad1499c51e22a1ec8b3fb43c53d4d1257a177c
MiroMuro <miro.laukkanen44@gmail.com>
2023-11-15 00:17:19 +0200

    added css to phonebook :)

--- osa_2/puhelinluettelo/src/App.jsx
+++ osa_2/puhelinluettelo/src/App.jsx
@@ -1,191 +1,213 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import phoneBookService from "./services/contacts"
+import { useState, useEffect } from "react";
+import phoneBookService from "./services/contacts";
 
-const Notification = ({message}) =>{
-  if(message === null){
-    return null
-  }else if(message.includes(" was already removed from the server!")){
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  } else if (message.includes(" was already removed from the server!")) {
     const errorStyle = {
-      color:'red',
-      backgroundColor: 'rgb(196, 192, 192)',
-      fontSize: 20,
-      borderStyle: "solid",
-      borderRadius: 5,
-      padding: 10,
-      marginBottom: 10
+      color: "red",
+    };
+    return <div style={errorStyle}>{message}</div>;
   }
-    return(
-      <div style={errorStyle}>
-      {message}
-    </div>
-    )
-  }
-  return(
-    <div className="notification">
-      {message}
-    </div>
-  )
-}
+  return <div className="notification">{message}</div>;
+};
 
-const Filter = ({searchWord, onChange}) =>{
-  return(
-    <div>Search for a contact
+const Filter = ({ searchWord, onChange }) => {
+  return (
+    <div className="search">
+      <br />
+      <h1>Search for a contact</h1>
+      <p className="searchText">Name</p>
       <input
+        className="searchInput"
+        placeholder="Contact name..."
         value={searchWord}
-        onChange={onChange}>
-      </input>
+        onChange={onChange}
+      ></input>
     </div>
-  )
-}
+  );
+};
 
-const PersonForm = (props) =>{
-  let name = props.contact.name
-  let number = props.contact.number
+const PersonForm = (props) => {
+  let name = props.contact.name;
+  let number = props.contact.number;
 
-  return(
-    <form onSubmit={props.addContact} >
-      <div>Name: <input
-      value={name} 
-      onChange={props.handleInputChange}/>
-      </div>
-      <div>Number: <input
-      value={number} 
-      onChange={props.handleNumberChange}/>
-      </div>
-      <button type="submit"> add</button>
-    </form>
-  )
-}
+  return (
+    <div className="form">
+      <form onSubmit={props.addContact}>
+        <div className="formDiv">
+          <p className="inputTitle">Name</p>
+          <input
+            className="contactInput"
+            placeholder="Contact name.."
+            value={name}
+            onChange={props.handleInputChange}
+          />
+        </div>
+        <div className="formDiv">
+          <p className="inputTitle">Number</p>
+          <input
+            className="contactInput"
+            placeholder="Contact number..."
+            value={number}
+            onChange={props.handleNumberChange}
+          />
+        </div>
+        <button type="submit" className="addButton">
+          {" "}
+          Add contact
+        </button>
+      </form>
+    </div>
+  );
+};
 
-const Contacts=({phoneBook,searchWord,onClick}) =>{
-    const filterd = phoneBook.filter(contact => contact.name.toLowerCase().includes(searchWord))
-    return(
-      filterd.map(((contact) => 
-      <div key={contact.id}>
-      <li >{contact.name} {contact.number} <button type='button' onClick={()=>onClick(contact.id)}>Delete</button></li>
-      </div>
-      ))
-    )
-  }
+const Contacts = ({ phoneBook, searchWord, onClick }) => {
+  const filterd = phoneBook.filter((contact) =>
+    contact.name.toLowerCase().includes(searchWord)
+  );
+  return filterd.map((contact) => (
+    <div className="contactDiv" key={contact.id}>
+      <li className="contactInstance">
+        {contact.name} {contact.number}{" "}
+        <button
+          type="button"
+          className="deleteButton"
+          onClick={() => onClick(contact.id)}
+        >
+          Delete
+        </button>
+      </li>
+    </div>
+  ));
+};
 
 const App = () => {
-  const [phoneBook, setPhonebook] = useState([])
+  const [phoneBook, setPhonebook] = useState([]);
   const [contact, setContact] = useState({
-    name : "", number : ""
-  })
-  const [searchWord, setSearchWord] = useState("")
-  const [message,setMessage] = useState(null)
+    name: "",
+    number: "",
+  });
+  const [searchWord, setSearchWord] = useState("");
+  const [message, setMessage] = useState(null);
 
-  const hook = () =>{
-    phoneBookService
-    .getAllContacts()
-    .then(initialContacts => {
-      setPhonebook(initialContacts)
-    })
-  }
-  useEffect(hook,[])
+  const hook = () => {
+    phoneBookService.getAllContacts().then((initialContacts) => {
+      setPhonebook(initialContacts);
+    });
+  };
+  useEffect(hook, []);
 
-  const addContact = (event) =>{
-    event.preventDefault()
-    
-        if(phoneBook.some(x => x.name === contact.name)){
-          if(confirm("Do you want to alter this persons phone number")===true){
-            let contactToUpdate = phoneBook.filter(y => y.name === contact.name)[0]
-            console.log(contactToUpdate)
-             phoneBookService
-             .updateContact(contactToUpdate.id, contact)
-             .then(updatedContact =>{
-              handleMessageChange(updatedContact," phone number updated")
-              setPhonebook(phoneBook.map(contact => contact.id !== updatedContact.id ? contact : updatedContact))
-             })
-             .catch(error=>{
-              handleMessageChange(contactToUpdate," was already removed from the server!")
-             })
-          }else{
-           null 
-          }
-          } else {
-        const newContactObject = {
-          name: contact.name,
-          number: contact.number
-        }
+  const addContact = (event) => {
+    event.preventDefault();
+
+    if (phoneBook.some((x) => x.name === contact.name)) {
+      if (confirm("Do you want to alter this persons phone number") === true) {
+        let contactToUpdate = phoneBook.filter(
+          (y) => y.name === contact.name
+        )[0];
+        console.log(contactToUpdate);
         phoneBookService
-        .createContact(newContactObject)
-        .then(initialContacts => {
-          console.log("Lisäys", initialContacts)
-          handleMessageChange(initialContacts," was added to the phonebook")
-          setPhonebook(phoneBook.concat(initialContacts))
-        })
+          .updateContact(contactToUpdate.id, contact)
+          .then((updatedContact) => {
+            handleMessageChange(updatedContact, " phone number updated");
+            setPhonebook(
+              phoneBook.map((contact) =>
+                contact.id !== updatedContact.id ? contact : updatedContact
+              )
+            );
+          })
+          .catch((error) => {
+            handleMessageChange(
+              contactToUpdate,
+              " was already removed from the server!"
+            );
+          });
+      } else {
+        null;
       }
-  }
-
-  const dc = (id) =>{
-    const personToBeDeleted = phoneBook.find((contact) => contact.id === id)
-    console.log(personToBeDeleted.name)
-    if(window.confirm(`Do you really want to delete ${personToBeDeleted.name}?`)){
+    } else {
+      const newContactObject = {
+        name: contact.name,
+        number: contact.number,
+      };
       phoneBookService
-    .deleteContact(id)
-    .then(response =>{
-      console.log(personToBeDeleted)
-      handleMessageChange(personToBeDeleted,
-         "was deleted from the phonebook")
-    })
-    setPhonebook(phoneBook.filter(contact => contact.id !== id))
-  }
+        .createContact(newContactObject)
+        .then((initialContacts) => {
+          console.log("Lisäys", initialContacts);
+          handleMessageChange(initialContacts, " was added to the phonebook");
+          setPhonebook(phoneBook.concat(initialContacts));
+        });
     }
-    
+  };
 
-  const handleMessageChange = (x,y,z) =>{
-    
-    setMessage(`${x.name} ${y}`)
-      setTimeout(()=>{
-        setMessage(null)
-      },3000)
-  }
-  
+  const dc = (id) => {
+    const personToBeDeleted = phoneBook.find((contact) => contact.id === id);
+    console.log("kssk");
+    if (
+      window.confirm(`Do you really want to delete ${personToBeDeleted.name}?`)
+    ) {
+      phoneBookService.deleteContact(id).then((response) => {
+        console.log(personToBeDeleted);
+        handleMessageChange(
+          personToBeDeleted,
+          "was deleted from the phonebook"
+        );
+      });
+      setPhonebook(phoneBook.filter((contact) => contact.id !== id));
+    }
+  };
 
-  const handleInputChange = (event) =>{
+  const handleMessageChange = (x, y, z) => {
+    setMessage(`${x.name} ${y}`);
+    setTimeout(() => {
+      setMessage(null);
+    }, 3000);
+  };
+
+  const handleInputChange = (event) => {
     const newContact = {
       name: event.target.value,
-      number : contact.number
-    }
-    setContact(newContact)
-
-    
-  }
-  const handleNumberChange = (event) =>{
+      number: contact.number,
+    };
+    setContact(newContact);
+  };
+  const handleNumberChange = (event) => {
     const newContact = {
       name: contact.name,
-      number : event.target.value
-    }
-    setContact(newContact)
-  }
-  const handleSearchWordChange = (event) =>{
-    setSearchWord(event.target.value)
-  }
-  
+      number: event.target.value,
+    };
+    setContact(newContact);
+  };
+  const handleSearchWordChange = (event) => {
+    setSearchWord(event.target.value);
+  };
+
   return (
-    <>
-      <h1>Phonebook</h1>
-      <Notification message={message}/>
-      <Filter searchWord={searchWord}
-        onChange={handleSearchWordChange}/>
+    <div>
+      <h1 className="title">Phonebook</h1>
+      <Notification message={message} />
+      <Filter searchWord={searchWord} onChange={handleSearchWordChange} />
       <h1>Add a new Contact</h1>
-      <PersonForm 
-      handleInputChange={handleInputChange}
-      handleNumberChange={handleNumberChange}
-      addContact={addContact}
-      contact={contact}
+      <PersonForm
+        handleInputChange={handleInputChange}
+        handleNumberChange={handleNumberChange}
+        addContact={addContact}
+        contact={contact}
       />
       <h1>Numbers:</h1>
       <div>
-      <ul>
-        <Contacts phoneBook={phoneBook} searchWord={searchWord} onClick={dc}/>
-      </ul>
+        <ul className="contactUl">
+          <Contacts
+            phoneBook={phoneBook}
+            searchWord={searchWord}
+            onClick={dc}
+          />
+        </ul>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
--- osa_2/puhelinluettelo/src/services/contacts.jsx
+++ osa_2/puhelinluettelo/src/services/contacts.jsx
@@ -1,23 +1,23 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+import axios from "axios";
+const baseUrl = "http://localhost:3001/persons";
 
-const createContact = newObject =>{
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => response.data)
-}
+const createContact = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
 
-const getAllContacts =()=> {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
-}
+const getAllContacts = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-const deleteContact = (id) =>{
-    const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then(response =>response.data)
-}
-const updateContact = (id, contact)=>{
-    const request = axios.put(`${baseUrl}/${id}`, contact)
-    return request.then(response => response.data)
-}
+const deleteContact = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+const updateContact = (id, contact) => {
+  const request = axios.put(`${baseUrl}/${id}`, contact);
+  return request.then((response) => response.data);
+};
 
-export default {createContact, getAllContacts, deleteContact, updateContact}
+export default { createContact, getAllContacts, deleteContact, updateContact };
