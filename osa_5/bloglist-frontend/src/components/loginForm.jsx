2317f0d1cb9a6e4ba4fa74a26b80c28888182286
MiroMuro <miro.laukkanen44@gmail.com>
2023-12-24 22:35:08 +0200

    5.12

--- osa_5/bloglist-frontend/src/components/loginForm.jsx
+++ osa_5/bloglist-frontend/src/components/loginForm.jsx
@@ -1,34 +1,46 @@
+import PropTypes from "prop-types";
+
 const loginForm = ({
   username,
   password,
   setUsername,
   setPassword,
   handleLogin,
-}) => (
-  <div>
-    <h1>Log in to application</h1>
-    <form onSubmit={handleLogin}>
-      <div>
-        username:
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        ></input>
-      </div>
-      <div>
-        password:
-        <input
-          type="text"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        ></input>
-      </div>
-      <button type="submit">Login</button>
-    </form>
-  </div>
-);
+}) => {
+  return (
+    <div>
+      <h1>Log in to application</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username:
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          ></input>
+        </div>
+        <div>
+          password:
+          <input
+            type="text"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          ></input>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+loginForm.propTypes = {
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  setUsername: PropTypes.func.isRequired,
+  setPassword: PropTypes.func.isRequired,
+  handleLogin: PropTypes.func.isRequired,
+};
 
 export default loginForm;
