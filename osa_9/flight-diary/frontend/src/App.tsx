c4d0ae4658c51cdda90c75270716db28ca53a78b
MiroMuro <miro.laukkanen44@gmail.com>
2024-02-26 12:00:50 +0200

    9.16-9.19

--- osa_9/flight-diary/frontend/src/App.tsx
+++ osa_9/flight-diary/frontend/src/App.tsx
@@ -1,8 +1,23 @@
 import { useState, useEffect } from "react";
-import { NonSensitiveDiaryEntry } from "./interfaces";
-import { getAllDiaries } from "./service/noteservice";
+import {
+  NonSensitiveDiaryEntry,
+  DiaryEntry,
+  Weather,
+  Visibility,
+} from "./interfaces";
+import { getAllDiaries, postDiary } from "./service/noteservice";
+import axios from "axios";
 const App = () => {
   const [entries, setEntries] = useState<NonSensitiveDiaryEntry[]>([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  //Id gets added in the backend automatically
+  const [newDiaryEntry, setNewDiaryEntry] = useState<DiaryEntry>({
+    id: undefined,
+    date: "",
+    weather: Weather.Sunny,
+    visibility: Visibility.Good,
+    comment: "",
+  });
 
   useEffect(() => {
     const getDiaries = async () => {
@@ -11,9 +26,146 @@ const App = () => {
     };
     getDiaries();
   }, []);
+
+  const submitEntry = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    //Type narrowing down to DiaryEntry type.
+    const res = await postDiary(newDiaryEntry);
+    if (axios.isAxiosError(res)) {
+      setErrorMessage(res.response?.data);
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 5000);
+    } else if (res === undefined) {
+      setErrorMessage("ERROR: undefined");
+    } else {
+      setEntries([...entries, res]);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewDiaryEntry({
+      ...newDiaryEntry,
+      [event.target.name]: event.target.value,
+    });
+    console.log(newDiaryEntry);
+  };
+
   return (
     <div>
       <div>Hello world!</div>
+      <div style={{ color: "red" }}>{errorMessage}</div>
+
+      <form onSubmit={submitEntry}>
+        <div>
+          Date:
+          <input
+            type="date"
+            name="date"
+            value={newDiaryEntry.date}
+            onChange={(event) => handleChange(event)}
+          />
+        </div>
+        <div>
+          Weather:
+          <span>
+            Sunny:
+            <input
+              defaultChecked
+              type="radio"
+              name="weather"
+              value={Weather.Sunny}
+              onChange={(event) => handleChange(event)}
+            />
+          </span>
+          <span>
+            Rainy:
+            <input
+              type="radio"
+              name="weather"
+              value={Weather.Rainy}
+              onChange={(event) => handleChange(event)}
+            />
+          </span>
+          <span>
+            Cloudy:
+            <input
+              type="radio"
+              name="weather"
+              value={Weather.Cloudy}
+              onChange={(event) => handleChange(event)}
+            />
+          </span>
+          <span>
+            Stormy
+            <input
+              type="radio"
+              name="weather"
+              value={Weather.Stormy}
+              onChange={(event) => handleChange(event)}
+            />
+          </span>
+          <span>
+            Windy:
+            <input
+              type="radio"
+              name="weather"
+              value={Weather.Windy}
+              onChange={(event) => handleChange(event)}
+            />
+          </span>
+        </div>
+        <span>
+          Visibility:
+          <span>
+            Great
+            <input
+              type="radio"
+              name="visibility"
+              value={Visibility.Great}
+              onChange={(event) => handleChange(event)}
+            />
+          </span>
+          <span>
+            Good
+            <input
+              defaultChecked
+              type="radio"
+              name="visibility"
+              value={Visibility.Good}
+              //onChange={() => handleVisibility(Visibility.Good)}
+              onChange={(event) => handleChange(event)}
+            />
+          </span>
+          <span>
+            Ok
+            <input
+              type="radio"
+              name="visibility"
+              value={Visibility.Ok}
+              onChange={(event) => handleChange(event)}
+            />
+          </span>
+          <span>
+            Poor
+            <input
+              type="radio"
+              name="visibility"
+              value={Visibility.Poor}
+              onChange={(event) => handleChange(event)}
+            />
+          </span>
+        </span>
+        <div>
+          Comment:
+          <input
+            name="comment"
+            value={newDiaryEntry.comment}
+            onChange={(event) => handleChange(event)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
       <ul>
         {entries.map((entry) => (
           <li id={String(entry.id)}>
--- osa_9/flight-diary/frontend/src/service/noteservice.ts
+++ osa_9/flight-diary/frontend/src/service/noteservice.ts
@@ -1,9 +1,20 @@
 import axios from "axios";
-import { NonSensitiveDiaryEntry } from "../interfaces";
+import { NonSensitiveDiaryEntry, DiaryEntry } from "../interfaces";
 
 const baseUrl = "http://localhost:3000/api/diaries";
 
 export const getAllDiaries = async () => {
   const res = await axios.get<NonSensitiveDiaryEntry[]>(baseUrl);
   return res.data;
+};
+
+export const postDiary = async (entry: DiaryEntry) => {
+  try {
+    const res = await axios.post<DiaryEntry>(baseUrl, entry);
+    return res.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      return error;
+    }
+  }
 };
