b18c2c2836fd42cca52b42985ca0f131cb4c04c0
MiroMuro <miro.laukkanen44@gmail.com>
2024-04-03 17:48:15 +0300

    8.9

--- osa_8/library-frontend/src/components/Books.js
+++ osa_8/library-frontend/src/components/Books.js
@@ -1,5 +1,13 @@
+import { ALL_BOOKS } from "./queries";
+import { useQuery } from "@apollo/client";
+
 const Books = (props) => {
-  const books = [];
+  const result = useQuery(ALL_BOOKS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+  const books = result.data.allBooks;
 
   return (
     <div>
--- osa_8/library-frontend/src/components/queries.js
+++ osa_8/library-frontend/src/components/queries.js
@@ -8,4 +8,14 @@ const ALL_AUTHORS = gql`
     }
   }
 `;
-export { ALL_AUTHORS };
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+      genres
+    }
+  }
+`;
+export { ALL_AUTHORS, ALL_BOOKS };
