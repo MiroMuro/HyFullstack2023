433cd64161fa27efa6c1f4b52d478a9bed5c9d97
ToriGrilli69 <miro.laukkanen44@gmail.com>
2024-04-07 13:43:44 +0300

    8.15

--- osa_8/index.ts
+++ osa_8/index.ts
@@ -1,6 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
+const { GraphQLError } = require("graphql");
 const { startStandaloneServer } = require("@apollo/server/standalone");
-import { Book } from "./types";
 //const { v4: uuidv4 } = require("uuid");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
@@ -21,98 +21,6 @@ mongoose
   .catch((error: any) => {
     console.log("Error connecring to MongoDB: ", error.message);
   });
-
-/*let authors = [
-  {
-    name: "Robert Martin",
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: "Martin Fowler",
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963,
-  },
-  {
-    name: "Fyodor Dostoevsky",
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821,
-  },
-  {
-    name: "Joshua Kerievsky", // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: "Sandi Metz", // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-];
-*/
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
- */
-
-let books: Book[] = [
-  {
-    title: "Clean Code",
-    published: 2008,
-    author: "Robert Martin",
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Agile software development",
-    published: 2002,
-    author: "Robert Martin",
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ["agile", "patterns", "design"],
-  },
-  {
-    title: "Refactoring, edition 2",
-    published: 2018,
-    author: "Martin Fowler",
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Refactoring to patterns",
-    published: 2008,
-    author: "Joshua Kerievsky",
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "patterns"],
-  },
-  {
-    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-    published: 2012,
-    author: "Sandi Metz",
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "design"],
-  },
-  {
-    title: "Crime and punishment",
-    published: 1866,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "crime"],
-  },
-  {
-    title: "The Demon ",
-    published: 1872,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "revolution"],
-  },
-];
 
 const typeDefs = `
   type Query {
@@ -191,7 +99,6 @@ const resolvers = {
           console.log(error);
           return null;
         }
-        return books.filter((book) => book.genres.includes(args.genre));
       }
 
       return await BookMongo.find({});
@@ -238,6 +145,20 @@ const resolvers = {
   },
   Mutation: {
     addBook: async (_root: any, args: any) => {
+      if (args.author.length < 4) {
+        throw new GraphQLError("Bad user input!", {
+          extensions: {
+            code: "Author name too short!",
+          },
+        });
+      }
+      if (args.title.length < 5) {
+        throw new GraphQLError("Bad user input!", {
+          extensions: {
+            code: "Book title too short!",
+          },
+        });
+      }
       try {
         let author = await AuthorMongo.findOne({ name: args.author });
         if (!author) {
@@ -252,7 +173,12 @@ const resolvers = {
         console.log("Book saved to DB");
         return book;
       } catch (error) {
-        console.log("Error saving book");
+        throw new GraphQLError("Creating book failed! ", {
+          extensions: {
+            code: "Bad user input.",
+            error,
+          },
+        });
       }
     },
     editAuthor: async (
@@ -265,10 +191,15 @@ const resolvers = {
           { $set: { born: args.setBornTo } },
           { returnDocument: "after" }
         );
+
         console.log("UPDATED AUTHOR: ", updatedAuthor);
         return updatedAuthor;
       } catch (error) {
-        console.log(error);
+        throw new GraphQLError("Editing user failed. ", {
+          extensions: {
+            code: "BAD_USER_INPUT || AUTHOR_NOT_FOUND",
+          },
+        });
       }
     },
   },
